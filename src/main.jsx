1184f3117ed63345708d873e1881d848ea23fc91
seungjinpark <seunginpark3365@gmail.com>
2024-10-12 14:51:49 +0900

    i18n 셋업

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import "./i18n.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
