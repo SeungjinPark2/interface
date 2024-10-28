7aea7b2e8ba0129de043bac670fa437f95f5a28d
SeungjinPark2 <seungjinpark3365@gmail.com>
2024-10-29 00:45:00 +0900

    add translation mx, us

--- src/i18n.js
+++ src/i18n.js
@@ -3,12 +3,28 @@ import { initReactI18next } from "react-i18next";
 import * as commonKo from "./translations/common/ko.json";
 import * as remittanceKo from "./translations/remittance/ko.json";
 import * as adminKo from "./translations/admin/ko.json";
+import * as commonMx from "./translations/common/mx.json";
+import * as remittanceMx from "./translations/remittance/mx.json";
+import * as adminMx from "./translations/admin/mx.json";
+import * as commonUs from "./translations/common/us.json";
+import * as remittanceUs from "./translations/remittance/us.json";
+import * as adminUs from "./translations/admin/us.json";
 
 const resources = {
   ko: {
     common: commonKo,
     remittance: remittanceKo,
     admin: adminKo,
+  },
+  mx: {
+    common: commonMx,
+    remittance: remittanceMx,
+    admin: adminMx,
+  },
+  us: {
+    common: commonUs,
+    remittance: remittanceUs,
+    admin: adminUs,
   },
 };
 
